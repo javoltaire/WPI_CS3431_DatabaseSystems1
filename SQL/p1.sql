94780b0a158611ec4dc959c45629b77e95614f97
Jules Voltaire <javoltaire@wpi.edu>
2016-09-24 19:17:16 -0400

    location table done

--- SQL/p1.sql
+++ SQL/p1.sql
@@ -4,16 +4,36 @@
 -- Table that will store the title acronyms and their value
 CREATE TABLE medical_title(
     acronym VARCHAR2(10) PRIMARY KEY,
-    title_name VARCHAR2(50)
+    title_name VARCHAR2(50) NOT NULL
 )
 
 -- Table that will hold a floor name and the building that it is in.
 -- Cannot have the two floors with the same name and building
 CREATE TABLE floor(
     floor_id INT PRIMARY KEY,
-    floor_name VARCHAR2(20),
-    floor_building VARCHAR2(20),
+    floor_name VARCHAR2(20) NOT NULL,
+    floor_building VARCHAR2(20) NOT NULL,
     CONSTRAINT uniqueFloor UNIQUE (floor_name, floor_building)
+)
+
+CREATE TABLE location(
+    location_id INT PRIMARY KEY,
+    x_coord NUMBER(4,0) NOT NULL,
+    y_coord NUMBER(4,0) NOT NULL,
+    location_name VARCHAR2(10),
+    location_type VARCHAR2(15),
+    floor INT NOT NULL,
+    CONSTRAINT uniqueCoord UNIQUE(xCoord, yCoord, floor),
+    CONSTRAINT check_location_type CHECK (location_type IN ('hallway', 'office', 'service_area')),
+    CONSTRAINT fk_floor FOREIGN KEY (floor) REFERENCES floor(floor_id)
+)
+
+CREATE TABLE location_neighbor(
+    id INT PRIMARY KEY,
+    location INT NOT NULL,
+    neighbor INT NOT NULL,
+    CONSTRAINT unique_location_neighbor UNIQUE(location, neighbor),
+    CONSTRAINT fk_location FOREIGN KEY (location) REFERENCES location(location_id)
 )
 
 CREATE TABLE health_care_provider(
@@ -32,19 +52,4 @@ CREATE TABLE Department(
     location INT,
     CONSTRAINT fk_dept_location FOREIGN KEY (location) REFERENCES location(location_id),
     CONSTRAINT check_dept_type CHECK (dept_type IN ('service', 'practice'))  
-)
-
-
-
-
-
-CREATE TABLE location(
-    location_id INT PRIMARY KEY,
-    x_coord NUMBER(4,0),
-    y_coord NUMBER(4,0),
-    location_name VARCHAR2(10),
-    location_type VARCHAR2(15),             -- Check one of three
-    
-    
-    CONSTRAINT uniqueCoord UNIQUE(xCoord, yCoord)
 )
