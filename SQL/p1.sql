7e8361208e6809fe6f7637ccd06ec95a65c1d8a5
Jules Voltaire <javoltaire@wpi.edu>
2016-09-23 20:26:17 -0400

    Started working on sql

--- SQL/p1.sql
+++ SQL/p1.sql
@@ -1,3 +1,10 @@
 /* Authors: Jules Voltaire - javoltaire and Ebenezer Ampiah - */
 
 -- Creating the tables
+CREATE TABLE HealthCareProviders (
+    providerID NUMBER,
+    firstName VARCHAR2,
+    lastName VARCHAR2,
+    providerTitle VARCHAR2,
+    location VARCHAR2
+)
