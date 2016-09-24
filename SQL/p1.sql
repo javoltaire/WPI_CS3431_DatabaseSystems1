28a8192d637c2d5ba5115c62995f7f0b3f337e5f
Jules Voltaire <javoltaire@wpi.edu>
2016-09-23 21:14:19 -0400

    little work done on sql commands

--- SQL/p1.sql
+++ SQL/p1.sql
@@ -1,6 +1,34 @@
 /* Authors: Jules Voltaire - javoltaire and Ebenezer Ampiah - */
 
 -- Creating the tables
+CREATE TABLE MedicalTitles (
+    acronym VARCHAR2(10) PRIMARY KEY,
+    titleName VARCHAR2(50)
+)
+
+CREATE TABLE Floors (
+    floorName VARCHAR2(20),
+    building VARCHAR2(20),
+    CONSTRAINT uniqueFloor UNIQUE (floorName, building)
+)
+
+CREATE TABLE Locations(
+    locattionID NUMBER(4,0) PRIMARY KEY,
+    xCoord NUMBER(4,0),
+    yCoord NUMBER(4,0),
+    locationName VARCHAR2(10),
+    
+    CONSTRAINT uniqueCoord UNIQUE(xCoord, yCoord)
+)
+
+
+
+
+
+
+
+
+
 CREATE TABLE HealthCareProviders (
     providerID NUMBER,
     firstName VARCHAR2,
