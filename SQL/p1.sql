7d61e378a284134c450fe61699e2ba1019b1c5f7
Jules Voltaire <javoltaire@wpi.edu>
2016-09-24 18:37:02 -0400

    health_care_provider, title and floor table done.

--- SQL/p1.sql
+++ SQL/p1.sql
@@ -1,11 +1,14 @@
 /* Authors: Jules Voltaire - javoltaire and Ebenezer Ampiah - */
 
 -- Creating the tables
+-- Table that will store the title acronyms and their value
 CREATE TABLE medical_title(
     acronym VARCHAR2(10) PRIMARY KEY,
     title_name VARCHAR2(50)
 )
 
+-- Table that will hold a floor name and the building that it is in.
+-- Cannot have the two floors with the same name and building
 CREATE TABLE floor(
     floor_id INT PRIMARY KEY,
     floor_name VARCHAR2(20),
@@ -14,10 +17,12 @@ CREATE TABLE floor(
 )
 
 CREATE TABLE location(
-    locattion_id INT PRIMARY KEY,
+    location_id INT PRIMARY KEY,
     x_coord NUMBER(4,0),
     y_coord NUMBER(4,0),
     location_name VARCHAR2(10),
+    location_type VARCHAR2(15),             -- Check one of three
+    
     
     CONSTRAINT uniqueCoord UNIQUE(xCoord, yCoord)
 )
@@ -30,10 +35,12 @@ CREATE TABLE location(
 
 
 
-CREATE TABLE HealthCareProviders (
-    providerID NUMBER,
-    firstName VARCHAR2,
-    lastName VARCHAR2,
-    providerTitle VARCHAR2,
-    location VARCHAR2
+CREATE TABLE health_care_provider(
+    provider_id NUMBER PRIMARY KEY,
+    first_name VARCHAR2(15) NOT NULL,
+    last_name VARCHAR2(20) NOT NULL,
+    title VARCHAR2(10) DEFAULT 'MD',
+    location NUMBER,
+    CONSTRAINT fk_title FOREIGN KEY (title) REFERENCES medical_title(acronym)
+    CONSTRAINT fk_location FOREIGN KEY (location) REFERENCES location(location_id)
 )
